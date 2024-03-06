c205c8b3b39e56e964075d4b2d98cb5322b89e84
Mark Verra <ma09verr@siena.edu>
2024-03-06 14:46:54 -0500

    Gotten first checkpoint for lab 7

--- App.js
+++ App.js
@@ -1,10 +1,37 @@
+import React, {useState} from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+
+//const Button = () => <View style={styles.button} />
 
 export default function App() {
+  const [count, setCount] = useState(0);
+  const onPress = () => {
+    setCount(prevCount => prevCount + 1);
+    console.log('Button Pressed');
+   };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style={styles.timeArea}>
+        <Text style={styles.timeText}>00:00:00</Text>
+      </View>
+        <TouchableOpacity style={styles.button} onPress={onPress}>
+        <Text style={styles.text}>Start</Text>
+        </TouchableOpacity>
+      <View style={styles.button}>
+        <Text style={styles.text}>Stop</Text>
+      </View>
+      <View style={styles.button}>
+        <Text style={styles.text}>Reset</Text>
+      </View>
+      <View style={styles.button}>
+        <Text style={styles.text}>Lap</Text>  
+      </View>
+      <View style={styles.lapTimes}>
+        <Text style={styles.text}>Lap Times</Text>
+
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +40,49 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#eef',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  timeArea: {
+    width: 250,
+    height: 75,
+    backgroundColor: 'blue',
+    alignItems: 'center',
+    justifyContent: 'center',
+    margin: 10,
+    padding: 10,
+  },
+  button: {
+    width: 100,
+    height: 50,
+    backgroundColor: 'purple',
+    alignItems: 'center',
+    justifyContent: 'center',
+    margin: 10,
+  },
+  timeText: {
+    fontSize: 36,
+    fontWeight: 'bold',
+    alignItems: 'center',
+    justifyContent: 'center',
+    color: 'white',
+  },
+  text: {
+    color: 'white',
+    fontSize: 24,
+    fontWeight: 'bold',
+    // alignContent: 'center',
+    // justifyContent: 'center',
+  },
+  lapTimes: {
+    //flex: 0.4,
+    width: 250,
+    height: 100,
+    backgroundColor: 'green',
+    alignItems: 'baseline stretch',
+    justifyContent: 'center',
+    margin: 10,
+    padding: 10,
+  }, 
 });
